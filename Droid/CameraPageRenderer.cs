71aa5b85fb3f76c31085b94f2a4f7b2e2930d641
dsouzajess98 <djessica@student.nitw.ac.in>
2018-07-12 11:53:04 +0300

    Initial commit

---
+++ Droid/CameraPageRenderer.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using CustomRenderer;
+using CustomRenderer.Droid;
+using Android.App;
+using Android.Content;
+using Android.Hardware;
+using Android.Views;
+using Android.Graphics;
+using Android.Widget;
+using System.Threading.Tasks;
+
+[assembly: ExportRenderer(typeof(CameraPage), typeof(CameraPageRenderer))]
+namespace CustomRenderer.Droid
+{
+    public class CameraPageRenderer : PageRenderer, TextureView.ISurfaceTextureListener
+    {
+        global::Android.Hardware.Camera camera;
+        global::Android.Widget.Button takePhotoButton;
+        global::Android.Widget.Button toggleFlashButton;
+        global::Android.Widget.Button switchCameraButton;
+        global::Android.Views.View view;
+
+        Activity activity;
+        CameraFacing cameraType;
+        TextureView textureView;
+        TextView result;
+        SurfaceTexture surfaceTexture;
+        readonly ImageClassifier imageClassifier = new ImageClassifier();
+        bool flashOn;
+
+        public CameraPageRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+
+            try
+            {
+                SetupUserInterface();
+                SetupEventHandlers();
+                AddView(view);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(@"			ERROR: ", ex.Message);
+            }
+        }
+
+        void SetupUserInterface()
+        {
+            activity = this.Context as Activity;
+            view = activity.LayoutInflater.Inflate(Resource.Layout.CameraLayout, this, false);
+            cameraType = CameraFacing.Back;
+
+            textureView = view.FindViewById<TextureView>(Resource.Id.textureView);
+            textureView.SurfaceTextureListener = this;
+        }
+
+        void SetupEventHandlers()
+        {
+            takePhotoButton = view.FindViewById<global::Android.Widget.Button>(Resource.Id.takePhotoButton);
+            result = view.FindViewById<global::Android.Widget.TextView>(Resource.Id.textView1);
+            takePhotoButton.Click += TakePhotoButtonTapped;
+
+            switchCameraButton = view.FindViewById<global::Android.Widget.Button>(Resource.Id.switchCameraButton);
+            switchCameraButton.Click += SwitchCameraButtonTapped;
+
+            toggleFlashButton = view.FindViewById<global::Android.Widget.Button>(Resource.Id.toggleFlashButton);
+            toggleFlashButton.Click += ToggleFlashButtonTapped;
+        }
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            base.OnLayout(changed, l, t, r, b);
+
+            var msw = MeasureSpec.MakeMeasureSpec(r - l, MeasureSpecMode.Exactly);
+            var msh = MeasureSpec.MakeMeasureSpec(b - t, MeasureSpecMode.Exactly);
+
+            view.Measure(msw, msh);
+            view.Layout(0, 0, r - l, b - t);
+        }
+
+        public void OnSurfaceTextureUpdated(SurfaceTexture surface)
+        {
+
+        }
+
+        public void OnSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
+        {
+            camera = global::Android.Hardware.Camera.Open((int)cameraType);
+            textureView.LayoutParameters = new FrameLayout.LayoutParams(width, height);
+            surfaceTexture = surface;
+
+            camera.SetPreviewTexture(surface);
+            PrepareAndStartCamera();
+        }
+
+        public bool OnSurfaceTextureDestroyed(SurfaceTexture surface)
+        {
+            camera.StopPreview();
+            camera.Release();
+            return true;
+        }
+
+        public void OnSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
+        {
+            PrepareAndStartCamera();
+        }
+
+        void PrepareAndStartCamera()
+        {
+            camera.StopPreview();
+
+            var display = activity.WindowManager.DefaultDisplay;
+            if (display.Rotation == SurfaceOrientation.Rotation0)
+            {
+                camera.SetDisplayOrientation(90);
+            }
+
+            if (display.Rotation == SurfaceOrientation.Rotation270)
+            {
+                camera.SetDisplayOrientation(180);
+            }
+
+            camera.StartPreview();
+        }
+
+        void ToggleFlashButtonTapped(object sender, EventArgs e)
+        {
+            flashOn = !flashOn;
+            if (flashOn)
+            {
+                if (cameraType == CameraFacing.Back)
+                {
+                    toggleFlashButton.SetBackgroundResource(Resource.Drawable.FlashButton);
+                    cameraType = CameraFacing.Back;
+
+                    camera.StopPreview();
+                    camera.Release();
+                    camera = global::Android.Hardware.Camera.Open((int)cameraType);
+                    var parameters = camera.GetParameters();
+                    parameters.FlashMode = global::Android.Hardware.Camera.Parameters.FlashModeTorch;
+                    camera.SetParameters(parameters);
+                    camera.SetPreviewTexture(surfaceTexture);
+                    PrepareAndStartCamera();
+                }
+            }
+            else
+            {
+                toggleFlashButton.SetBackgroundResource(Resource.Drawable.NoFlashButton);
+                camera.StopPreview();
+                camera.Release();
+
+                camera = global::Android.Hardware.Camera.Open((int)cameraType);
+                var parameters = camera.GetParameters();
+                parameters.FlashMode = global::Android.Hardware.Camera.Parameters.FlashModeOff;
+                camera.SetParameters(parameters);
+                camera.SetPreviewTexture(surfaceTexture);
+                PrepareAndStartCamera();
+            }
+        }
+
+        void SwitchCameraButtonTapped(object sender, EventArgs e)
+        {
+            if (cameraType == CameraFacing.Front)
+            {
+                cameraType = CameraFacing.Back;
+
+                camera.StopPreview();
+                camera.Release();
+                camera = global::Android.Hardware.Camera.Open((int)cameraType);
+                camera.SetPreviewTexture(surfaceTexture);
+                PrepareAndStartCamera();
+            }
+            else
+            {
+                cameraType = CameraFacing.Front;
+
+                camera.StopPreview();
+                camera.Release();
+                camera = global::Android.Hardware.Camera.Open((int)cameraType);
+                camera.SetPreviewTexture(surfaceTexture);
+                PrepareAndStartCamera();
+            }
+        }
+        public Android.Graphics.Bitmap ToGrayscale(Android.Graphics.Bitmap bmpOriginal)
+        {
+            int width, height;
+            height = bmpOriginal.Height;
+            width = bmpOriginal.Width;
+
+            float[] mat = new float[]{
+            0.3f, 0.59f, 0.11f, 0, 0,
+            0.3f, 0.59f, 0.11f, 0, 0,
+            0.3f, 0.59f, 0.11f, 0, 0,
+            0, 0, 0, 1, 0,};
+
+            Android.Graphics.Bitmap bmpGrayscale = Android.Graphics.Bitmap.CreateBitmap(width, height, Android.Graphics.Bitmap.Config.Argb8888);
+            GC.Collect();
+            Android.Graphics.Canvas c = new Android.Graphics.Canvas(bmpGrayscale);
+            Android.Graphics.ColorMatrixColorFilter filter = new Android.Graphics.ColorMatrixColorFilter(mat);
+            Android.Graphics.Paint paint = new Android.Graphics.Paint();
+            paint.SetColorFilter(filter);
+            c.DrawBitmap(bmpOriginal, 0, 0, paint);
+            return bmpGrayscale;
+        }
+        async void TakePhotoButtonTapped(object sender, EventArgs e)
+        {
+            camera.StopPreview();
+
+            var image = textureView.Bitmap;
+            image=ToGrayscale(image);
+            image = Bitmap.CreateBitmap(image, 200, 200, 500, 500);
+            var x = Task.Run(() => imageClassifier.RecognizeImage(image));
+            result.Text = x.Result;
+            try
+            {
+                var absolutePath = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDcim).AbsolutePath;
+                var folderPath = absolutePath + "/Camera";
+                var filePath = System.IO.Path.Combine(folderPath, string.Format("photo_{0}.jpg", Guid.NewGuid()));
+
+                var fileStream = new FileStream(filePath, FileMode.Create);
+                await image.CompressAsync(Bitmap.CompressFormat.Jpeg, 50, fileStream);
+                fileStream.Close();
+                image.Recycle();
+
+                var intent = new Android.Content.Intent(Android.Content.Intent.ActionMediaScannerScanFile);
+                var file = new Java.IO.File(filePath);
+                var uri = Android.Net.Uri.FromFile(file);
+                intent.SetData(uri);
+                MainActivity.Instance.SendBroadcast(intent);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(@"				", ex.Message);
+            }
+
+            camera.StartPreview();
+        }
+    }
+}
+
---
+++ Droid/ImageClassifier.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Android.App;
+using Android.Graphics;
+using Org.Tensorflow.Contrib.Android;
+using System.Threading.Tasks;
+namespace CustomRenderer.Droid
+{
+    public class ImageClassifier 
+    {
+        readonly List<string> labels;
+        readonly TensorFlowInferenceInterface inferenceInterface;
+
+        public ImageClassifier()
+        {
+            var assets = Application.Context.Assets;
+            inferenceInterface = new TensorFlowInferenceInterface(assets, "model.pb");
+            using (var sr = new StreamReader(assets.Open("labels.txt")))
+            {
+                var content = sr.ReadToEnd();
+                labels = content.Split('\n').Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s)).ToList();
+            }
+
+        }
+
+        static readonly int InputSize = 227;
+        static readonly string InputName = "Placeholder";
+        static readonly string OutputName = "loss";
+
+        public string RecognizeImage(Bitmap bitmap)
+        {            
+          //  Console.WriteLine("Hello image");
+            var outputNames = new[] { OutputName };            
+            var floatValues = GetBitmapPixels(bitmap);
+          //  System.Diagnostics.Debug.Write("jqdkjqhej " + floatValues);
+            var outputs = new float[labels.Count];
+
+            inferenceInterface.Feed(InputName, floatValues, 1, InputSize, InputSize, 3);
+            inferenceInterface.Run(outputNames);
+            inferenceInterface.Fetch(OutputName, outputs);
+
+            var results = new List<Tuple<float, string>>();
+    
+            for (var i = 0; i < outputs.Length; ++i)
+            {
+                results.Add(Tuple.Create(outputs[i], labels[i]));
+           //     System.Diagnostics.Debug.WriteLine(outputs[i] + " " + labels[i]);
+
+            }
+    
+            string v= results.OrderByDescending(t => t.Item1).First().Item2;
+            System.Diagnostics.Debug.WriteLine("jqks " + v);
+            
+            return v;
+        }
+
+        static float[] GetBitmapPixels(Bitmap bitmap)
+        {
+            var floatValues = new float[227 * 227 * 3];
+
+            using (var scaledBitmap = Bitmap.CreateScaledBitmap(bitmap, 227, 227, false))
+            {
+                using (var resizedBitmap = scaledBitmap.Copy(Bitmap.Config.Argb8888, false))
+                {
+                    var intValues = new int[227 * 227];
+                    resizedBitmap.GetPixels(intValues, 0, resizedBitmap.Width, 0, 0, resizedBitmap.Width, resizedBitmap.Height);
+
+                    for (int i = 0; i < intValues.Length; ++i)
+                    {
+                        var val = intValues[i];
+
+                        floatValues[i * 3 + 0] = ((val & 0xFF));// - 104);
+                        floatValues[i * 3 + 1] = (((val >> 8) & 0xFF));// - 117);
+                        floatValues[i * 3 + 2] = (((val >> 16) & 0xFF));// - 123);
+                    }
+
+                    resizedBitmap.Recycle();
+                }
+
+                scaledBitmap.Recycle();
+            }
+
+            return floatValues;
+        }
+    }
+}
